c89e03be1b6ec23356a41d551bba7014725a0f21
Kevin Lufkin <klufkin11@gmail.com>
2016-04-26 20:01:50 -0400

    Comments on general layout of files base on components

--- data.js
+++ data.js
@@ -1,3 +1,17 @@
 var data = {
   /* Add your data here */
+  /*----- General & Examples (Component 1) -----*/
+   name: ".sort()",
+   description: "The sort() method sorts the elements of an array in place and returns the array. The sort is not necessarily stable. The default sort order is according to string Unicode code points.",
+   examples: [
+    {
+      title: "First Example Title",
+      code: "var arr = []; ... "
+    }
+  ]
+
+/*----- Section Containers (Component 2) -----*/
+/*----- Browser Compatibility (Component 3) -----*/
+/*----- Footnotes & Reference (Component 4) -----*/
+/*----- Sidebar (Component 5) -----*/
 };
